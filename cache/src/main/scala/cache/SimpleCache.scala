4e41efa9814e62ce00f9276118eae9c57da1512d
Yuriy Malkov <yura345@gmail.com>
2024-03-24 13:48:19 -0400

    simple design

--- cache/src/main/scala/cache/SimpleCache.scala
+++ cache/src/main/scala/cache/SimpleCache.scala
@@ -1,5 +1,20 @@
 package main.scala.cache
 
+class SimpleCache {
+  def addHash(): Unit = {
+    println("adding hash to cache")
+  }
+
+  def readHash(): Unit = {
+    println("returning data from hash")
+  }
+}
+
 object SimpleCache extends App {
   println("Welcome to my Cache implementation")
+
+  val cache = new SimpleCache()
+
+  cache.addHash()
+  cache.readHash()
 }
